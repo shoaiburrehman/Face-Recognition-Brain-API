9927f8dfa216c17134393ef0f9cf03b7548d8c83
Shoaib Ur Rehman Khan <39486938+shoaiburrehman@users.noreply.github.com>
2019-12-11 14:54:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'wantpeace',
-      database : 'smartbrain'
+      host : '*********',
+      user : '********',
+      password : '*********',
+      database : '**********'
     }
 });
 
